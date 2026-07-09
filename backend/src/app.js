816075c5bd45a8b06a505c49af94c99e1da242ef
Nishantd17 <nishantd265@gmail.com>
2026-07-09 13:02:48 +0530

    chore: enable production CORS and API URL for Vercel

--- backend/src/app.js
+++ backend/src/app.js
@@ -13,20 +13,8 @@ app.use(helmet({
 }));
 
 // CORS Configuration
-const allowedOrigins = ['http://localhost:5173', 'http://127.0.0.1:5173'];
-if (process.env.FRONTEND_URL) {
-  allowedOrigins.push(process.env.FRONTEND_URL);
-  allowedOrigins.push(process.env.FRONTEND_URL.replace(/\/$/, ''));
-}
-
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
